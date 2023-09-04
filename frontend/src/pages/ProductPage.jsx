89ccfcdceafe10e0f8cb455812515931dc5e96d0
kimsoyol <naywinlhaing@gmail.com>
2023-09-05 04:16:55 +0700

    connect to MongoDB

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import products from "../products";
 import { Link } from "react-router-dom";
 import {
   Row,
@@ -11,10 +10,21 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ProductPage = () => {
+  const [product, setProduct] = useState({})
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    }
+
+    fetchProduct()
+  }, [productId])
 
   return (
     <>
