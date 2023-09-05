305cc1ef34d76f54692f960db558689032247d43
kimsoyol <naywinlhaing@gmail.com>
2023-09-06 05:10:18 +0700

    add logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,7 +39,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // POST  /api/users/logout  Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+  
+  res.status(200).json({message: 'Logged out successfully'});
 });
 
 // GET  /api/users/profile  Private
