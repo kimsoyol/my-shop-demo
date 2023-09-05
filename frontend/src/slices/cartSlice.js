5cc445a5707e0c453d6ccd12b31dd9bb65f43d3d
kimsoyol <naywinlhaing@gmail.com>
2023-09-05 22:32:23 +0700

    add cart page

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,12 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
-};
+
 
 const cartSlice = createSlice({
   name: "cart",
@@ -24,23 +23,8 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
 
-      // Calculate items price
-      state.itemPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-
-      // Calculate shipping price (If order is over $100 then free, else $10 shipping )
-      state.shippingPrice = addDecimals(state.itemPrice > 100 ? 0 : 10)
-
-      // Calculate tax price
-      state.taxPrice = addDecimals(Number((0.15 * state.itemPrice).toFixed(2)))
-
-      // Calculate total price
-      state.totalPrice = (
-        Number(state.itemPrice) + Number(state.shippingPrice) + Number(state.taxPrice)
-      ).toFixed(2)
-
-      localStorage.setItem('cart', JSON.stringify(state))
+      return updateCart(state)
+     
     },
   },
 });
