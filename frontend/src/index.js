477d3fbf8f5a05a710bc9d4f11b4eb1f468b44a8
kimsoyol <naywinlhaing@gmail.com>
2023-09-06 20:30:11 +0700

    add login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import CartPage from './pages/CartPage';
+import LoginPage from './pages/LoginPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomePage />}/>
       <Route path='/product/:id' element={<ProductPage />} />
       <Route path='/cart' element={<CartPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Route>
   )
 )
