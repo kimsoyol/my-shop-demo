17701715b883753497758297eeba02ed569d66f9
kimsoyol <naywinlhaing@gmail.com>
2023-09-08 14:13:00 +0700

    add update user profile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -40,8 +40,7 @@ const Header = () => {
             <Nav className="ms-auto">
               <LinkContainer to="/cart">
                 <Nav.Link>
-                  <FaShoppingCart /> 
-                   Cart
+                  <FaShoppingCart /> Cart
                   {cartItems.length > 0 && (
                     <Badge pill bg="success" style={{ marginLeft: "5px" }}>
                       {cartItems.reduce((a, c) => a + c.qty, 0)}
