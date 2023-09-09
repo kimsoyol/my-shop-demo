dc092378b4440c8d7f1bda7d9e9e08ef40a44e39
kimsoyol <naywinlhaing@gmail.com>
2023-09-09 22:51:35 +0700

    add create review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -61,17 +61,61 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 // Delete /api/product/:id  Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
-
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    await Product.deleteOne({_id: product._id})
-    res.status.json({ message: "Product deleted"})
+    await Product.deleteOne({ _id: product._id });
+    res.status.json({ message: "Product deleted" });
   } else {
     res.status(404);
     throw new Error("Resource not found");
   }
 });
 
+// POST /api/product/:id/review  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
 
-export { getProducts, getProductById, createProduct, updateProduct, deleteProduct };
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+
+    await product.save()
+
+    res.status(200).json({ message: "Reiew added" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+};
